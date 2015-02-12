3795ccf0c8c577cd888de178a79ff7d63167e537
dimanov <dimanov@gmail.com>
2015-02-12 15:48:21 +0200

    Add projects

---
+++ AllocateArray/AllocateArray.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Problem 1. Allocate array
+ * Write a program that allocates array of  20  integers and initializes each element by its index multiplied by  5 .
+ * Print the obtained array on the console.
+ */
+
+using System;
+
+namespace AllocateArray
+{
+    class AllocateArray
+    {
+        static void Main()
+        {
+            int[] myArray = new int[20];
+
+            for (int i = 0; i < 20; i++ )
+            {
+                myArray[i] = i * 5;
+            }
+            string numbersString = string.Join(" ", myArray);
+
+            Console.WriteLine(numbersString);
+        }
+    }
+}
